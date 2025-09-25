1b865b9b197dcd512b71d589ad7241ed578fedf7
Rocco Moresi <137230323+roccomoresi@users.noreply.github.com>
2025-09-24 21:37:50 -0300

    fix some shi

--- src/main/java/com/uade/tpo/gimnasio/services/impl/EmailServiceImpl.java
+++ src/main/java/com/uade/tpo/gimnasio/services/impl/EmailServiceImpl.java
@@ -4,28 +4,27 @@ import com.uade.tpo.gimnasio.services.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
-    private static Logger log = LoggerFactory.getLogger(EmailServiceImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(EmailServiceImpl.class);
 
-    private MailSender mailSender;
+    private final JavaMailSender mailSender;
 
-    public EmailServiceImpl(MailSender mailSender) {
+    public EmailServiceImpl(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     @Override
     public void send(SimpleMailMessage msg) {
         try {
-            this.mailSender.send(msg);
-        }
-        catch (MailException ex) {
-            log.error(ex.getMessage(), ex);
+            mailSender.send(msg);
+        } catch (MailException ex) {
+            log.error("Error enviando email: {}", ex.getMessage(), ex);
         }
     }
 }
