5b48fbdbf0c840733e19c552fe6aafff6b9f3600
gydanel <alan.gagliero@gmail.com>
2025-11-23 19:59:21 -0300

    Add sede to reservas

--- src/main/java/com/uade/tpo/gimnasio/repositories/ReservaRepository.java
+++ src/main/java/com/uade/tpo/gimnasio/repositories/ReservaRepository.java
@@ -1,14 +1,21 @@
 package com.uade.tpo.gimnasio.repositories;
 
+import com.uade.tpo.gimnasio.dto.CourseProjection;
+import com.uade.tpo.gimnasio.dto.ReservaProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.uade.tpo.gimnasio.models.entity.Reserva;
 import com.uade.tpo.gimnasio.models.entity.Reserva.Estado;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 import java.util.List;
 
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+@RepositoryRestResource(collectionResourceRel = "reservas", path = "reservations", excerptProjection = ReservaProjection.class)
+public interface ReservaRepository extends CrudRepository<Reserva, Long> {
     // boolean existsByUsuario_IdAndTurno_Id(Long usuarioId, Long turnoId);
 
     List<Reserva> findByUsuario_IdAndEstadoInOrderByIdDesc(Long usuarioId, Collection<Estado> estados);
@@ -16,7 +23,8 @@ public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     // para próximas reservas del usuario (ej: estado CONFIRMADA)
     List<Reserva> findByUsuario_IdAndEstadoOrderByIdDesc(Long usuarioId, Estado estado);
 
-    List<Reserva> findByUsuarioId(Long usuarioId);
+    @RestResource(path = "byUser", rel = "byUser")
+    List<Reserva> findByUsuarioId(@Param("userId") Long usuarioId);
 
     List<Reserva> findByCourseId(Long courseId);
     long countByCourseId(Long courseId);
